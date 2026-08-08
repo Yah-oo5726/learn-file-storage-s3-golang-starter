02640f973bc475c3a902d196f1dfa61d77c0b795
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-08-08 16:15:17 -0700

    added aspect ratios to s3 bucket link for sorting

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,21 +1,74 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
+	"math"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 
-	"crypto/rand"
-	"encoding/base64"
-
 	s3 "github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+type ffprobeOutput struct {
+	Streams []struct {
+		Width  int `json:"width"`
+		Height int `json:"height"`
+	} `json:"streams"`
+}
+
+func getVideoAspectRatio(filepath string) (string, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filepath)
+	stdoutBuffer := bytes.Buffer{}
+	cmd.Stdout = &stdoutBuffer
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	var output ffprobeOutput
+	err = json.Unmarshal(stdoutBuffer.Bytes(), &output)
+	if err != nil {
+		return "", fmt.Errorf("failed to unmarshal ffprobe output: %w", err)
+	}
+
+	if len(output.Streams) == 0 {
+		return "", fmt.Errorf("no video streams found")
+	}
+
+	width := output.Streams[0].Width
+	height := output.Streams[0].Height
+	var aspectRatio string
+	if height == 0 {
+		return "", fmt.Errorf("height is zero, cannot calculate aspect ratio")
+	}
+	if width == 0 {
+		return "", fmt.Errorf("width is zero, cannot calculate aspect ratio")
+	}
+	landscapeCloseness := math.Abs(float64(width*9 - height*16))
+	if landscapeCloseness <= 200 {
+		aspectRatio = "16:9"
+	}
+	portraitCloseness := math.Abs(float64(width*16 - height*9))
+	if portraitCloseness <= 200 {
+		aspectRatio = "9:16"
+	}
+	if aspectRatio == "" {
+		aspectRatio = "other"
+	}
+
+	return aspectRatio, nil
+}
 
 func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
 	r.Body = http.MaxBytesReader(w, r.Body, 10<<30) // 1 GB limit
@@ -86,10 +139,26 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't generate filename", err)
 		return
 	}
+	aspectRatio, err := getVideoAspectRatio(tempFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to get video aspect ratio", err)
+		return
+	}
+	var aspectRatioPrefix string
+	if aspectRatio != "16:9" && aspectRatio != "9:16" {
+		aspectRatioPrefix = "other"
+	}
+	if aspectRatio == "16:9" {
+		aspectRatioPrefix = "landscape"
+	}
+	if aspectRatio == "9:16" {
+		aspectRatioPrefix = "portrait"
+	}
 	filename := base64.RawURLEncoding.EncodeToString(filenameBytes)
+	filenameWithPrefix := fmt.Sprintf("%s/%s.mp4", aspectRatioPrefix, filename)
 	_, err = cfg.s3Client.PutObject(context.Background(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
-		Key:         &filename,
+		Key:         &filenameWithPrefix,
 		Body:        tempFile,
 		ContentType: &fileType,
 	})
@@ -97,7 +166,7 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusInternalServerError, "Failed to upload video to S3", err)
 		return
 	}
-	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, filename)
+	videoURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, filenameWithPrefix)
 	video.VideoURL = &videoURL
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
